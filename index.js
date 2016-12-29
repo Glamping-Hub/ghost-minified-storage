74e949b15ff4846df3af3491e4d9782485ef404f
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2016-12-29 14:05:08 +0100

    Resize, optimize, and save originals images

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 
 'use strict';
 
+var path = require('path');
+var fs = require('fs');
 var util = require('util');
 var BaseStore = require('../../../core/server/storage/base');
 var config = require('../../../core/server/config');
@@ -27,12 +29,11 @@ function MinifyStore(config) {
 util.inherits(MinifyStore, BaseStore);
 
 MinifyStore.prototype.constructor = function (storageConfig) {
+    // Configure next storage instance
     if (!storageConfig || !storageConfig.hasOwnProperty('nextStorage')) {
         throw 'You must configure the "nextStorage" property for the "Ghost Minified Storage"!';
     }
-
     var NextStorage;
-
     try {
         // CASE: load adapter from custom path  (.../content/storage)
         NextStorage = require(config.paths.storagePath.custom + storageConfig.nextStorage);
@@ -42,11 +43,27 @@ MinifyStore.prototype.constructor = function (storageConfig) {
             throw err.message;
         }
 
-        // CASE: either storage[storageChoice] is already set or why check for in the default storage path
+        // CASE: load adapter from default path
         NextStorage = require(config.paths.storagePath.default + storageConfig.nextStorage);
     }
-
     this.nextStorageInstance = new NextStorage(config.storage[storageConfig.nextStorage]);
+
+    // Configure each property
+    var that = this;
+    [
+        {name: 'saveOriginalFile', defaultValue: 'true'},
+        {name: 'deleteOriginalFile', defaultValue: true},
+        {name: 'maxWidth', defaultValue: false},
+        {name: 'maxHeight', defaultValue: false},
+        {name: 'regExWithoutSuffix', defaultValue: /^.+(\.[^\.]+)$/},
+        {name: 'regExResultWithoutSuffix', defaultValue: '$1$2'},
+        {name: 'regExWithSuffix', defaultValue: /^.+_resized(\.[^\.]+)$/},
+        {name: 'regExResultWithSuffix', defaultValue: '$1_resized$2'}
+    ].forEach(function (property) {
+        that[property.name] = storageConfig.hasOwnProperty(property.name)
+            ? storageConfig[property.name]
+            : property.defaultValue;
+    });
 };
 
 MinifyStore.prototype.save = function (file, targetDir) {
@@ -64,54 +81,86 @@ MinifyStore.prototype.save = function (file, targetDir) {
     <   context: { user: 1, client: null } }
     */
 
-    var nextStorageInstance = this.nextStorageInstance;
+    var that = this;
+    var resizeImageIfNeeds = function (src, resolve, reject) {
+        try {
+            // Resize image
+            var buffer = sharp(src)
+                .png({
+                    progressive: true,
+                    compressionLevel: 9
+                })
+                .jpeg({
+                    quality: 80,
+                    progressive: true,
+                    optimizeScans: true
+                });
 
-    return new Promise(function (resolve, reject) {
-        //TODO Save original image if needs
-
-        tmp.file(function (err, tmpFilePath, ignore, cleanupCallback) {
+            if (that.maxWidth || that.maxHeight) {
+                buffer = buffer.resize(1024, 1024).max();
+            }
+            buffer.toBuffer(function (err, buffer, info) {
+                if (err) {
+                    throw err;
+                }
+                resolve(buffer, info);
+            });
+        } catch (e) {
+            reject(e);
+        }
+    };
+    var minifyImage = function (buffer, resolve, reject) {
+        tmp.file(function (err, temporalFilePath, ignore, cleanupCallback) {
             if (err) {
                 throw err;
             }
 
-            // Resize image
-            sharp(file.path)
-                .resize(1024, 1024)
-                .max()
-                .toFile(tmpFilePath, function (err, ignore) {
-                    if (err) {
-                        throw err;
-                    }
+            try {
+                imagemin.buffer(buffer, {
+                    plugins: [
+                        imageminGifsicle(),
+                        imageminJpegtran(),
+                        imageminOptipng()
+                    ]
+                }).then(function (buffer) {  // files = [{data: <Buffer 89 50 4e …>, path: 'build/images/foo.jpg'}, …]
+                    fs.writeFile(temporalFilePath, buffer, function (err) {
+                        if (err) {
+                            throw err;
+                        }
 
-                    // Minify image
-                    //FIXME: Remove hardcoded "/tmp/"
-                    imagemin([tmpFilePath], '/tmp/', {
-                        plugins: [
-                            imageminGifsicle(),
-                            imageminJpegtran(),
-                            imageminOptipng()
-                        ]
-                    }).then(function (files) {
-                        // console.log(files);
-                        //   => [{data: <Buffer 89 50 4e …>, path: 'build/images/foo.jpg'}, …]
+                        var newFileObject = JSON.parse(JSON.stringify(file)); // JS clone stuff
+                        newFileObject.originalname = file.originalname.replace(that.regExWithoutSuffix, that.regExResultWithSuffix);
+                        newFileObject.filename = path.basename(temporalFilePath);
+                        newFileObject.path = temporalFilePath;
+                        newFileObject.size = buffer.length;
 
-                        var newFileObject = JSON.parse(JSON.stringify(file));
-                        //newFileObject.filename = '';
-                        newFileObject.path = files[0].path;
-                        //delete newFileObject.size;
-
-                        nextStorageInstance
-                            .save(newFileObject, targetDir)
-                            .then(function (image) {
-                                resolve(image);
-                                cleanupCallback();
-                            });
-                    }).catch(function (error) {
-                        reject(error);
-                        cleanupCallback();
+                        resolve(newFileObject)
+                            .then(cleanupCallback, cleanupCallback)
+                            .catch(cleanupCallback);
                     });
                 });
+            } catch (e) {
+                cleanupCallback();
+                reject(e);
+            }
         });
+    };
+
+    return new Promise(function (resolve, reject) {
+        // Save original image if needs
+        if (that.saveOriginalFile) {
+            that.nextStorageInstance.save(file, targetDir);
+        }
+
+        resizeImageIfNeeds(file.path, function (buffer) {
+            minifyImage(buffer, function (file) {
+                return that.nextStorageInstance
+                    .save(file, targetDir)
+                    .then(function (file2) {
+                        resolve(file2);
+                    });
+            }, reject);
+        }, reject);
     });
 };
 
@@ -123,9 +172,15 @@ MinifyStore.prototype.serve = function () {
     return this.nextStorageInstance.serve.apply(this.nextStorageInstance, arguments);
 };
 
-MinifyStore.prototype.delete = function () {
-    //TODO Delete original image if needs
-    return this.nextStorageInstance.delete.apply(this.nextStorageInstance, arguments);
+MinifyStore.prototype.delete = function (fileName, targetDir) {
+    var promises = [this.nextStorageInstance.delete.apply(this.nextStorageInstance, arguments)];
+
+    // Delete original image if needs
+    if (this.deleteOriginalFile) {
+        promises.push(this.nextStorageInstance.delete(fileName.replace(this.regExWithSuffix, this.regExResultWithoutSuffix), targetDir));
+    }
+
+    return Promise.all(promises);
 };
 
 module.exports = MinifyStore;
